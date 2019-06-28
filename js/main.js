d468cde5c2d8c3623099d9af515f40b236bf6397
dylan weaver <dweaver@umd.edu>
2019-06-28 00:00:22 -0400

    new and improved with dynamic location support

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// Put divs and stuff here for global use (bad, but who cares)
+let capitalizeWords = str => str.split(' ')
+								.map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+								.join(' ');
+
+async function getLocation() {
+	// Get the users location for displaying and getting weather
+	let locationAPI = 'https://www.geoip-db.com/json/';
+
+	let response = await fetch(locationAPI);
+	let json = await response.json();
+
+	return [json['city'], json['postal'], json['country_code']];
+}
+
+function getWeatherIcon(weatherID) {
+	return weatherIcons[weatherID].icon;
+}
+
+async function setWeather(city, zip, country) {
+	let tempAndLocation = document.getElementById("temp-location");
+	let weatherIcon = document.getElementById("weather-icon");
+	let weather = document.getElementById("weather");
+
+	let weatherAPI = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&units=imperial&appid=e5b292ae2f9dae5f29e11499c2d82ece`;
+
+	let response = await(fetch(weatherAPI));
+	let json = await(response.json());
+	let temp = Math.round(json.main.temp);
+	let weatherID = json.weather[0].id;
+	let weatherDescription = capitalizeWords(json.weather[0].description);
+
+	let icon = getWeatherIcon(weatherID);
+	let iconClass = `wi wi-${icon}`;
+
+	tempAndLocation.innerHTML = `It's ${temp}°F in ${city}`;
+	weatherIcon.className += iconClass;
+	weather.innerHTML = `${weatherDescription}`;
+
+	// Update every 10 minutes
+	setTimeout(setWeather, 600000, city, country);
+}
+
+function formatTime(time) {
+	// If element < 10: prints "0x:" instead of just "x:"
+	return time < 10 ? `0${time}` : `${time}`;
+}
+
+function getDayOfWeek(date) {
+	let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+	return days[date.getDay()];
+}
+
+function setDateTime() {
+	let date = new Date();
+	let sec = formatTime(date.getSeconds());
+	let min = formatTime(date.getMinutes());
+	let hour = formatTime(date.getHours());
+	let dayOfWeek = getDayOfWeek(date);
+	let dateTimeElement = document.getElementById("datetime");
+
+	dateTimeElement.innerHTML = `${dayOfWeek}, ${date.toLocaleDateString()} - ${hour}:${min}:${sec}`;
+
+	setTimeout(setDateTime, 1000);
+}
+
+async function main() {
+	try {
+		let [city, zip, country] = await getLocation();
+		await setWeather(city, zip, country);
+	} catch (e) {
+		// Error with weather section.. just empty it.
+		console.log(e);
+		document.getElementById("weather-text").innerHTML = "";
+	}
+
+	setDateTime();
+}
+
+main();
---
+++ js/weather.js
@@ -0,0 +1,222 @@
+let weatherIcons = {
+	"200": {
+		"label": "thunderstorm with light rain",
+		"icon": "storm-showers"
+	},
+	"201": {
+		"label": "thunderstorm with rain",
+		"icon": "storm-showers"
+	},
+	"202": {
+		"label": "thunderstorm with heavy rain",
+		"icon": "thunderstorm"
+	},
+	"210": {
+		"label": "light thunderstorm",
+		"icon": "storm-showers"
+	},
+	"211": {
+		"label": "thunderstorm",
+		"icon": "thunderstorm"
+	},
+	"212": {
+		"label": "heavy thunderstorm",
+		"icon": "thunderstorm"
+	},
+	"221": {
+		"label": "ragged thunderstorm",
+		"icon": "thunderstorm"
+	},
+	"230": {
+		"label": "thunderstorm with light drizzle",
+		"icon": "storm-showers"
+	},
+	"231": {
+		"label": "thunderstorm with drizzle",
+		"icon": "storm-showers"
+	},
+	"232": {
+		"label": "thunderstorm with heavy drizzle",
+		"icon": "storm-showers"
+	},
+	"300": {
+		"label": "light intensity drizzle",
+		"icon": "sprinkle"
+	},
+	"301": {
+		"label": "drizzle",
+		"icon": "sprinkle"
+	},
+	"302": {
+		"label": "heavy intensity drizzle",
+		"icon": "sprinkle"
+	},
+	"310": {
+		"label": "light intensity drizzle rain",
+		"icon": "sprinkle"
+	},
+	"311": {
+		"label": "drizzle rain",
+		"icon": "sprinkle"
+	},
+	"312": {
+		"label": "heavy intensity drizzle rain",
+		"icon": "sprinkle"
+	},
+	"313": {
+		"label": "shower rain and drizzle",
+		"icon": "sprinkle"
+	},
+	"314": {
+		"label": "heavy shower rain and drizzle",
+		"icon": "sprinkle"
+	},
+	"321": {
+		"label": "shower drizzle",
+		"icon": "sprinkle"
+	},
+	"500": {
+		"label": "light rain",
+		"icon": "rain"
+	},
+	"501": {
+		"label": "moderate rain",
+		"icon": "rain"
+	},
+	"502": {
+		"label": "heavy intensity rain",
+		"icon": "rain"
+	},
+	"503": {
+		"label": "very heavy rain",
+		"icon": "rain"
+	},
+	"504": {
+		"label": "extreme rain",
+		"icon": "rain"
+	},
+	"511": {
+		"label": "freezing rain",
+		"icon": "rain-mix"
+	},
+	"520": {
+		"label": "light intensity shower rain",
+		"icon": "showers"
+	},
+	"521": {
+		"label": "shower rain",
+		"icon": "showers"
+	},
+	"522": {
+		"label": "heavy intensity shower rain",
+		"icon": "showers"
+	},
+	"531": {
+		"label": "ragged shower rain",
+		"icon": "showers"
+	},
+	"600": {
+		"label": "light snow",
+		"icon": "snow"
+	},
+	"601": {
+		"label": "Snow",
+		"icon": "snow"
+	},
+	"602": {
+		"label": "Heavy Snow",
+		"icon": "snow"
+	},
+	"611": {
+		"label": "Sleet",
+		"icon": "sleet"
+	},
+	"612": {
+		"label": "Light shower sleet",
+		"icon": "sleet"
+	},
+	"613": {
+		"label": "Shower sleet",
+		"icon": "sleet"
+	},
+	"615": {
+		"label": "Light rain and snow",
+		"icon": "rain-mix"
+	},
+	"616": {
+		"label": "Rain and snow",
+		"icon": "rain-mix"
+	},
+	"620": {
+		"label": "Light shower snow",
+		"icon": "rain-mix"
+	},
+	"621": {
+		"label": "Shower snow",
+		"icon": "rain-mix"
+	},
+	"622": {
+		"label": "Heavy shower snow",
+		"icon": "rain-mix"
+	},
+	"701": {
+		"label": "mist",
+		"icon": "sprinkle"
+	},
+	"711": {
+		"label": "Smoke",
+		"icon": "smoke"
+	},
+	"721": {
+		"label": "Haze",
+		"icon": "day-haze"
+	},
+	"731": {
+		"label": "sand/ dust whirls",
+		"icon": "sandstorm"
+	},
+	"741": {
+		"label": "fog",
+		"icon": "fog"
+	},
+	"751": {
+		"label": "sand",
+		"icon": "sandstorm"
+	},
+	"761": {
+		"label": "dust",
+		"icon": "dust"
+	},
+	"762": {
+		"label": "volcanic ash",
+		"icon": "smog"
+	},
+	"771": {
+		"label": "squalls",
+		"icon": "windy"
+	},
+	"781": {
+		"label": "tornado",
+		"icon": "tornado"
+	},
+	"800": {
+		"label": "clear sky",
+		"icon": "day-sunny"
+	},
+	"801": {
+		"label": "few clouds: 11-25%",
+		"icon": "cloud"
+	},
+	"802": {
+		"label": "scattered clouds: 25-50%",
+		"icon": "cloudy"
+	},
+	"803": {
+		"label": "broken clouds: 51-84%",
+		"icon": "cloudy"
+	},
+	"804": {
+		"label": "overcast clouds: 85-100%",
+		"icon": "cloudy"
+	}
+}
